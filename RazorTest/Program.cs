d2504b275ba192c2ace2e91d63d9ca0a9e3d20d1
ckiick1234 <ckiick1234@gmail.com>
2025-09-15 13:54:03 -0700

    Add key vault again

--- RazorTest/Program.cs
+++ RazorTest/Program.cs
@@ -13,30 +13,30 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 
-//if (!builder.Environment.IsProduction())
-//{
-//    var keyVaultURL = builder.Configuration.GetSection("KeyVault:KeyVaultURL");
-//    var keyVaultClientId = builder.Configuration.GetSection("KeyVault:ClientId");
-//    var keyVaultClientSecret = builder.Configuration.GetSection("KeyVault:ClientSecret");
-//    var keyVaultDirectoryId = builder.Configuration.GetSection("KeyVault:DirectoryId");
+if (!builder.Environment.IsProduction())
+{
+    var keyVaultURL = builder.Configuration.GetSection("KeyVault:KeyVaultURL");
+    var keyVaultClientId = builder.Configuration.GetSection("KeyVault:ClientId");
+    var keyVaultClientSecret = builder.Configuration.GetSection("KeyVault:ClientSecret");
+    var keyVaultDirectoryId = builder.Configuration.GetSection("KeyVault:DirectoryId");
 
-//    var credential = new ClientSecretCredential(
-//        keyVaultDirectoryId.Value!.ToString() ?? throw new InvalidOperationException("KeyVaultDirectoryId is not configured."),
-//        keyVaultClientId.Value!.ToString() ?? throw new InvalidOperationException("KeyVaultClientId is not configured."),
-//        keyVaultClientSecret.Value!.ToString() ?? throw new InvalidOperationException("KeyVaultClientSecret is not configured.")
-//    );
+    var credential = new ClientSecretCredential(
+        keyVaultDirectoryId.Value!.ToString() ?? throw new InvalidOperationException("KeyVaultDirectoryId is not configured."),
+        keyVaultClientId.Value!.ToString() ?? throw new InvalidOperationException("KeyVaultClientId is not configured."),
+        keyVaultClientSecret.Value!.ToString() ?? throw new InvalidOperationException("KeyVaultClientSecret is not configured.")
+    );
 
-//    var client = new SecretClient(new Uri(keyVaultURL.Value!.ToString()),credential);
+    var client = new SecretClient(new Uri(keyVaultURL.Value!.ToString()),credential);
     
-//    builder.Services.AddDbContext<ProductContext>(options =>
-//        options.UseCosmos(
-//            client.GetSecret("CosmosDbConnectionString").Value.Value.ToString(),
-//            client.GetSecret("CosmosDbContainerName").Value.Value.ToString()
-//        )
-//    );
-//}
-//else
-//{
+    builder.Services.AddDbContext<ProductContext>(options =>
+        options.UseCosmos(
+            client.GetSecret("CosmosDbConnectionString").Value.Value.ToString(),
+            client.GetSecret("CosmosDbContainerName").Value.Value.ToString()
+        )
+    );
+}
+else
+{
     builder.Configuration.AddUserSecrets<Program>();
     var cosmosConnectionString = builder.Configuration["CosmosDbConnectionString"];
     var cosmosDatabaseName = builder.Configuration["CosmosDbContainerName"];
